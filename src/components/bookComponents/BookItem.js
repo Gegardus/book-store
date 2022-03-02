2c48197ff977da6d39176ea3718658f535e6c5d8
Gegardus <stgegard@gmail.com>
2022-03-03 01:54:39 +0400

    Update styles

--- src/components/bookComponents/BookItem.js
+++ src/components/bookComponents/BookItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import { removeBookApi } from '../../redux/books/books';
 
 const BookItem = (props) => {
@@ -13,17 +14,46 @@ const BookItem = (props) => {
   const removeBookFromStore = () => {
     dispatch(removeBookApi(id));
   };
+
+  const percentage = 66;
   return (
     <div className="book-container">
       <div className="title-container">
         <h3 className="book-title">
           {title}
         </h3>
-        <button className="remove-btn" type="button" onClick={removeBookFromStore}>Remove</button>
+        <h3 className="book-author">
+          {category}
+        </h3>
+        <div className="book-btns">
+          <button className="remove-btn" type="button">Comments</button>
+          <button className="remove-btn" type="button" onClick={removeBookFromStore}>Remove</button>
+          <button className="remove-btn" type="button">Edit</button>
+        </div>
       </div>
-      <h3 className="book-author">
-        {category}
-      </h3>
+      <div className="progress-bar-c">
+        <CircularProgressbar
+          value={percentage}
+          styles={buildStyles({ pathColor: '#085c83', marginRight: '10px' })}
+          className="progress-bar"
+        />
+        <div className="progress-value-c">
+          <p className="progress-percentage">
+            {percentage}
+            %
+          </p>
+          <p className="progress-completed">Completed</p>
+        </div>
+      </div>
+      <div className="update-progg-c">
+        <h2 className="chapter">CURRENT CHAPTER</h2>
+        <h3 className="chapterth">
+          Chapter
+          {' '}
+          { `${Math.floor(Math.random() * 50)}`}
+        </h3>
+        <button type="button" className="update-progg-btn">Update Progress</button>
+      </div>
     </div>
   );
 };
